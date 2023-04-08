e0cb8492ec4a2b71d8c0ae01776ff65f61b94d22
Rishvic Pushpakaran <rishvic@gmail.com>
2023-04-08 18:48:04 +0530

    Added B+Tree with insert

--- src/main/java/xyz/rishvic/app/BPlusTreeApp.java
+++ src/main/java/xyz/rishvic/app/BPlusTreeApp.java
@@ -1,8 +1,12 @@
 package xyz.rishvic.app;
 
 import java.util.concurrent.Callable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import xyz.rishvic.util.BPlusTree;
 
 @Command(
     name = "b+demo",
@@ -11,9 +15,29 @@ import picocli.CommandLine.Command;
     description = "A CLI application demonstrating B+ Tree operations.")
 public class BPlusTreeApp implements Callable<Integer> {
 
+  final Logger logger = LoggerFactory.getLogger(BPlusTreeApp.class);
+
+  @Option(
+      names = {"-B", "--branching-factor"},
+      description = "Branching factor of B+ Tree",
+      defaultValue = "3")
+  int bf;
+
   @Override
   public Integer call() throws Exception {
-    System.out.println("Hello World!");
+    logger.info("Hello World!");
+    BPlusTree<Integer> tree;
+    try {
+      tree = new BPlusTree<>(bf);
+    } catch (IllegalArgumentException err) {
+      logger.error("Illegal argument passed", err);
+      return 1;
+    }
+    for (int elem :
+        new int[] {
+          36, 92, 93, 63, 69, 38, 44, 60, 23, 45, 36, 25, 31, 94, 29, 91, 9, 9, 80, 65, 23
+        }) tree.add(elem);
+    System.out.println(tree.toPrettyString());
     return 0;
   }
 
---
+++ src/main/java/xyz/rishvic/util/BPlusTree.java
@@ -0,0 +1,135 @@
+package xyz.rishvic.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.tuple.Pair;
+
+public class BPlusTree<E extends Comparable<E>> {
+
+  private final int bf;
+  private Node<E> root;
+
+  public BPlusTree() {
+    bf = 3;
+    root = new Node<>(null, null, true);
+  }
+
+  public BPlusTree(int bf) throws IllegalArgumentException {
+    if (bf < 3) throw new IllegalArgumentException("Branching factor must be at least 3");
+    this.bf = bf;
+    this.root = new Node<>(null, null, true);
+  }
+
+  public void add(E element) {
+    addRecursive(root, element);
+    if (root.items.size() < bf) return;
+
+    Pair<E, Node<E>> splitRes = root.splitMid();
+    assert splitRes != null : "root.splitMid() returned `null`";
+    E midElement = splitRes.getLeft();
+    Node<E> splitRoot = splitRes.getRight();
+
+    Node<E> newRoot = new Node<>(null, null, false);
+    newRoot.items.add(midElement);
+    newRoot.children.add(root);
+    newRoot.children.add(splitRoot);
+
+    root = newRoot;
+  }
+
+  private void addRecursive(Node<E> node, E element) {
+    int at;
+    for (at = 0; at < node.items.size(); at++) {
+      if (node.items.get(at).compareTo(element) >= 0) break;
+    }
+
+    if (node.isLeaf()) {
+      if (!node.items.contains(element)) node.items.add(at, element);
+      return;
+    }
+
+    Node<E> child = node.children.get(at);
+    addRecursive(child, element);
+    if (child.items.size() < bf) return;
+
+    Pair<E, Node<E>> splitRes = child.splitMid();
+    assert splitRes != null : "child.splitMid() returned `null`";
+    E midElement = splitRes.getLeft();
+    Node<E> splitChild = splitRes.getRight();
+
+    node.items.add(at, midElement);
+    node.children.add(at + 1, splitChild);
+  }
+
+  public String toPrettyString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append(root.items).append('\n');
+    root.toPrettyStringChildren(sb, " ");
+    return sb.toString();
+  }
+
+  private static class Node<E extends Comparable<E>> {
+    List<E> items;
+    Node<E> next, prev;
+    List<Node<E>> children;
+
+    Node(Node<E> prev, Node<E> next, boolean isLeaf) {
+      this.items = new ArrayList<>();
+      this.next = next;
+      this.prev = prev;
+      this.children = isLeaf ? null : new ArrayList<Node<E>>();
+    }
+
+    boolean isLeaf() {
+      return children == null;
+    }
+
+    Pair<E, Node<E>> splitMid() {
+      if (items.size() < 2) return null;
+
+      int midIndex = items.size() / 2;
+      E midElement = items.get(midIndex);
+
+      Node<E> splitNode = new Node<>(this, next, isLeaf());
+      this.next = splitNode;
+      if (splitNode.next != null) splitNode.next.prev = splitNode;
+
+      List<E> splitItems = items.subList(isLeaf() ? midIndex : midIndex + 1, items.size());
+      splitNode.items.addAll(splitItems);
+      splitItems.clear();
+      if (!isLeaf()) items.remove(items.size() - 1);
+
+      if (!isLeaf()) {
+        List<Node<E>> splitChildren = children.subList(midIndex + 1, children.size());
+        splitNode.children.addAll(splitChildren);
+        splitChildren.clear();
+      }
+
+      return Pair.of(midElement, splitNode);
+    }
+
+    private void toPrettyStringChildren(StringBuilder sb, String prefix) {
+      if (isLeaf()) return;
+
+      for (int index = 0; index < children.size(); index++) {
+        boolean isLastChild = index == children.size() - 1;
+        sb.append(prefix)
+            .append(isLastChild ? "└─" : "├─")
+            .append(children.get(index).items)
+            .append('\n');
+        children
+            .get(index)
+            .toPrettyStringChildren(sb, isLastChild ? prefix + "   " : prefix + "│  ");
+      }
+    }
+
+    @Override
+    public String toString() {
+      return new ToStringBuilder(this)
+          .append("items", items)
+          .append("children", children)
+          .toString();
+    }
+  }
+}
